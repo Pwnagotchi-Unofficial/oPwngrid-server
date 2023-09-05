6242cf95f24aff3fcc69e65fd8247d8e204dba4e
XxKingsxX-Pinu <Kieran@digitalrealism.com.au>
2023-09-05 19:14:31 +1000

    Update CreateMessages.sql

--- DB_Schema/CreateMessages.sql
+++ DB_Schema/CreateMessages.sql
@@ -5,7 +5,6 @@ CREATE TABLE messages (
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
     seen_at TIMESTAMP,
-	SenderID VARCHAR(255),
 	receiver VARCHAR(255),
     sender_name VARCHAR(255) NOT NULL,
     sender VARCHAR(255) NOT NULL,
