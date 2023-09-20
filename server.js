22706592defb5acc122d92c4fb6df641b76a60a1
XxKingsxX-Pinu <Kieran@digitalrealism.com.au>
2023-09-20 14:44:50 +1000

    little fixes and improvs

--- server.js
+++ server.js
@@ -260,7 +260,7 @@ app.get('/api/v1/search/:fingerprint', authenticate, (req, res) => {
   //https://pwnagotchi.ai/api/grid/#get-api-v1-unit-fingerprint
   console.log('Got web search for ' + req.params.fingerprint)
   //Query fingerprint via mysql
-  connection.query('SELECT created_at,updated_at,country,name,identity,data,public_key FROM units WHERE identity = ? LIMIT 1',
+  connection.query('SELECT created_at,updated_at,country,name,identity,data,public_key, (SELECT COUNT(aps.identity) FROM aps WHERE identity = ?) AS amount FROM units WHERE identity = ? LIMIT 1;  ',
   [req.params.fingerprint],
   function(err, results, fields) {
     if (err) {
