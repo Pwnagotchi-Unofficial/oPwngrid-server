42691e589a08f7cb2cf1e11023ee50be1869775b
N3ttX <n3ttx0@gmail.com>
2023-10-04 10:24:02 +0200

    Moved static routes to it's own file

--- server.js
+++ server.js
@@ -106,35 +106,8 @@ app.get("*", function(req, res, next){
     res.status(404).json({"error":"Not Found"});
 });
 
-
-
-
-
-
-//routing for main website
-app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-app.get("/search/*", (req, res) => {
-    res.sendFile(__dirname + "/public/search.html");
-});
-app.get("/search", (req, res) => {
-    res.sendFile(__dirname + "/public/search.html");
-});
-app.get("/convert", (req, res) => {
-    res.sendFile(__dirname + "/public/convert.html");
-});
-app.get("/css/:file", (req, res) => {
-    res.sendFile(__dirname + `/public/css/${req.params.file}`);
-});
-app.get("/leaderboard", (req, res) => {
-    res.sendFile(__dirname + "/public/leaderboard.html");
-});
-app.get("/statistics", (req, res) => {
-    res.sendFile(__dirname + "/public/statistics.html");
-});
-
+// all routes leading to static content here
+require("./routes/static_routes.js")(app);
 
 //----------------------------------------------------------------------------
 //Start of statistic APIs for major web pages
