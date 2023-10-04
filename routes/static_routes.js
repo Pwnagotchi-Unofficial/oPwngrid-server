ea81327302fed77c0beb4baf3bab8fa8df461853
N3ttX <n3ttx0@gmail.com>
2023-10-04 16:08:35 +0200

    Static routes fixed
    Forgot to commit this when frontend was still relevant in here, shouldn't be necessary anymore but..

--- routes/static_routes.js
+++ routes/static_routes.js
@@ -2,26 +2,26 @@ const express = require("express");
 
 module.exports = function(app) {
     //routing for main website
-    app.use(express.static(__dirname + "/public"));
+    app.use(express.static(process.env.PWD + "/public"));
     app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/public/index.html");
+        res.sendFile(process.env.PWD + "/public/index.html");
     });
     app.get("/search/*", (req, res) => {
-        res.sendFile(__dirname + "/public/search.html");
+        res.sendFile(process.env.PWD + "/public/search.html");
     });
     app.get("/search", (req, res) => {
-        res.sendFile(__dirname + "/public/search.html");
+        res.sendFile(process.env.PWD + "/public/search.html");
     });
     app.get("/convert", (req, res) => {
-        res.sendFile(__dirname + "/public/convert.html");
+        res.sendFile(process.env.PWD + "/public/convert.html");
     });
     app.get("/css/:file", (req, res) => {
-        res.sendFile(__dirname + `/public/css/${req.params.file}`);
+        res.sendFile(process.env.PWD + `/public/css/${req.params.file}`);
     });
     app.get("/leaderboard", (req, res) => {
-        res.sendFile(__dirname + "/public/leaderboard.html");
+        res.sendFile(process.env.PWD + "/public/leaderboard.html");
     });
     app.get("/statistics", (req, res) => {
-        res.sendFile(__dirname + "/public/statistics.html");
+        res.sendFile(process.env.PWD + "/public/statistics.html");
     });
 };
