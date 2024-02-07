adc916f72ed73d13deca10576d3efbb49c513ca8
CyberArtemio <156206062+cyberartemio@users.noreply.github.com>
2024-02-07 18:07:35 +0100

    removed middlewares not working and renamed `ENVIRONMENT` env var to `NODE_ENV`

--- server.js
+++ server.js
@@ -14,39 +14,7 @@ console.log(`[START] enviroment: ${process.env.ENVIROMENT}`)
 // use custom logger and set up CORS
 app.use(utils.log)
 
-// setup depending on dev and prod, like strict domain checking etc
-if (process.env.ENVIROMENT === 'dev') {
-  // dev enviroment
-  app.get('*', function (req, res, next) {
-    // if it's a sub-domain
-    next()
-  })
-} else {
-  // prod enviroment
-  app.use(cors({ origin: ['https://opwngrid.xyz', 'https://api.opwngrid.xyz'] }))
-
-  app.get('*', function (req, res, next) {
-    // if it's a sub-domain
-    if (req.headers.host === 'opwngrid.xyz') {
-      if (req.url.includes('/api/')) {
-        res.send('API not here')
-        return
-      } else {
-        // next();
-        return
-      }
-    } else if (req.headers.host === 'api.opwngrid.xyz') {
-      if (req.url.includes('/api/')) {
-        next()
-        return
-      } else {
-        res.status(404).json({ error: 'Not Found' })
-        return
-      }
-    }
-    res.status(404).json({ error: 'Not Found' })
-  })
-}
+if (process.env.NODE_ENV === 'production') { app.use(cors({ origin: ['https://opwngrid.xyz', 'https://api.opwngrid.xyz'] })) }
 
 app.use('/api/v1/units', routes.units)
 app.use('/api/v1/unit', routes.unit)
@@ -54,7 +22,14 @@ app.use('/api/v1/uptime', routes.uptime)
 app.use('/api/v1/total', routes.total)
 app.use('/api/v1/recent', routes.recent)
 app.use('/api/v1/search', routes.search)
-app.use('/api/statistics', routes.statistics) // TODO: doesn't follow APIs logic -> should be changed to /api/v1/statistics
+app.use('/api/statistics', routes.statistics) // FIXME: doesn't follow APIs logic -> should be changed to /api/v1/statistics
+
+// 404 not found
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not Found'
+  })
+})
 
 // App Listen ---------------------------
 app.listen(port, () => {
